5cb6ce4b4fcd776f9445302c95e4b903e7b8fc08
Alan Kang <alankang@fortinet.com>
2017-09-20 13:49:16 -0700

    temp checkin

--- main.go
+++ main.go
@@ -2,33 +2,16 @@ package main
 
 import (
     "fmt"
-    "flag"
     "strings"
     "path/filepath"
     "os"
     "io"
     "io/ioutil"
     "sync"
-    "crypto/md5"
-    "hash/fnv"
     "time"
+    "crypto/md5"
     "github.com/dustin/go-humanize"
-    //"github.com/codahale/blake2"
 )
-
-type fpaths []string
-
-func (ps *fpaths) String() string {
-    return fmt.Sprintf("%v", *ps)
-}
-
-func (ps *fpaths) Set(v string) error {
-    p := strings.Split(v, ",")
-    for _, s := range p {
-        *ps = append(*ps, s)
-    }
-    return nil
-}
 
 type fileMeta struct {
     fullpath, basename string
@@ -43,30 +26,11 @@ func (fm *fileMeta) getKeyString() string {
 
 type fileMap map[string][]*fileMeta
 
-var allFiles = make(fileMap)
-var totalSize int64
-var numFiles int64
 var wg sync.WaitGroup
 var sem = make(chan struct{}, 20) //number of files opened concurrently
-var wait1 = make(chan struct{})
-var wait2 = make(chan struct{})
-
-var mydirs fpaths
-var verbose = flag.Bool("v", false, "verbose output")
-var keyops = flag.String("o", "",
-                    `compare options seperated by ','
-            Options:
-              name  file basename (included by default)
-              size  file size (included by default)
-              hash  md5 hash of the file`)
 
 func main() {
-    flag.Var(&mydirs, "d", "directory to scan")
-    flag.Parse()
-
-    if len(mydirs) == 0 {
-        mydirs = fpaths{"."}
-    }
+    parseCmdLine()
 
     start := time.Now()
 
@@ -75,6 +39,12 @@ func main() {
         wg.Add(1)
         go walkDir(dir, filesCh)
     }
+
+    var allFiles = make(fileMap)
+    var totalSize int64
+    var numFiles int64
+    var wait1 = make(chan struct{})
+    var wait2 = make(chan struct{})
 
     // collector routine
     go func() {
@@ -185,19 +155,11 @@ func getFileMeta(fpath string, fi os.FileInfo) (*fileMeta, error) {
     }
     defer f.Close()
 
-    if true {
-        h := fnv.New64()
-        if _, err := io.CopyN(h, f, n); err != nil {
-            return nil, err
-        }
-        fm.hash = fmt.Sprintf("%x", h.Sum64())
-    } else {
-        h := md5.New()
-        if _, err := io.CopyN(h, f, n); err != nil {
-            return nil, err
-        }
-        fm.hash = fmt.Sprintf("%x", h.Sum(nil))
+    h := md5.New()
+    if _, err := io.CopyN(h, f, n); err != nil {
+        return nil, err
     }
+    fm.hash = fmt.Sprintf("%x", h.Sum(nil))
 
     return &fm, nil
 }
